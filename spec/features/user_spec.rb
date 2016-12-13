85fac1b18926fe8a5c9ac55fe5a33b26917e3565
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-13 13:03:33 -0800

    deleted comments

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -167,34 +167,4 @@ feature 'Secrets#index' do
 
   end
 
-  # save_and_open_page
-  # context "with improper credentials" do
-  #   before do
-  #     user.email = user.email + "x"
-  #     sign_in(user)
-  #   end
-  #
-  #   scenario "does not allow sign in" do
-  #     expect(page).to have_content "We couldn't sign you in"
-  #   end
-  # end
-  #
-  # context "with proper credentials" do
-  #   before do
-  #     sign_in(user)
-  #   end
-  #   scenario "successfully signs in an existing user" do
-  #     # verify we're on the user's show page now
-  #     expect(page).to have_content "You've successfully signed in"
-  #   end
-  #
-  #   context "after signing out" do
-  #     before do
-  #       sign_out
-  #     end
-  #     scenario "signs out the user" do
-  #       expect(page).to have_content "You've successfully signed out"
-  #     end
-  #   end
-  # end
 end
